794065e4e56437f46f5a2da601b1830fb92bc9a0
fotjus <valkman0@gmail.com>
2016-10-23 20:47:12 +0200

    Test
    Testowy komentarz

--- app/src/main/java/com/civicproject/civicproject/MainActivity.java
+++ app/src/main/java/com/civicproject/civicproject/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//TEST PUSH - SZYMON
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
