9cc7b8daa0ce61adbe16b41d3094d8b4f2a1f348
patrycjush <p.zajaq@gmail.com>
2016-12-29 12:15:12 +0100

    Nic ważnego.

--- app/src/main/java/com/civicproject/civicproject/Validator.java
+++ app/src/main/java/com/civicproject/civicproject/Validator.java
@@ -8,10 +8,24 @@ public class Validator {
     private Pattern pattern;
     private Matcher matcher;
 
-    final String EMAIL_PATTERN ="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final String EMAIL_PATTERN ="^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+    private static final String PHONENUMBER_PATTERN = "\\d{3}-\\d{3}-\\d{3}";
 
     public boolean emailValidator(String input) {
         pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    public boolean passwordValidator(String input) {
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(input);
+        return matcher.matches();
+    }
+
+    public boolean phoneNumberValidator(String input) {
+        pattern = Pattern.compile(PHONENUMBER_PATTERN);
         matcher = pattern.matcher(input);
         return matcher.matches();
     }
