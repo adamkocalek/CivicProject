b396674c8fc0da63d7cb97ca2c12421ffad047ef
adamkocalek <adam.kocalek@gmail.com>
2016-12-31 16:00:51 +0100

    Top 10 lista, ale dziala po ponownym zalogowaniu nie wiem czemu..

--- app/src/main/java/com/civicproject/civicproject/Fragments/PopularItems_Fragment.java
+++ app/src/main/java/com/civicproject/civicproject/Fragments/PopularItems_Fragment.java
@@ -2,10 +2,15 @@ package com.civicproject.civicproject.Fragments;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -16,6 +21,8 @@ import android.widget.Toast;
 
 import com.civicproject.civicproject.Downloader;
 import com.civicproject.civicproject.ListViewAdapter;
+import com.civicproject.civicproject.ListViewAdapterUser;
+import com.civicproject.civicproject.MyFTPClientFunctions;
 import com.civicproject.civicproject.Parser;
 import com.civicproject.civicproject.ProjectActivity;
 import com.civicproject.civicproject.R;
@@ -28,6 +35,8 @@ import java.util.Collections;
 import static com.civicproject.civicproject.R.id.listViewMyProjects;
 
 public class PopularItems_Fragment extends Fragment {
+    private Parser parser = null;
+    int temp;
 
     @Nullable
     @Override
@@ -36,12 +45,27 @@ public class PopularItems_Fragment extends Fragment {
         View view = inflater.inflate(R.layout.popular_frag_layout, null);
 
         final ListView listView_popular = (ListView) view.findViewById(R.id.listView_popular);
+        final ArrayList<Integer> indexs = new ArrayList<>(), tempList2 = new ArrayList<>();
+        final ArrayList<String> popularList = new ArrayList<String>(), ids = new ArrayList<>(), authors = new ArrayList<>(), likes = new ArrayList<>(), dates = new ArrayList<>(), images = new ArrayList<>();
+        parser = new Parser();
 
+        for (int i = 0; i < parser.subjects.size(); i++) {
+            tempList2.add(Integer.parseInt(parser.likes.get(i)));
+        }
 
+        Collections.sort(tempList2);
+        Collections.reverse(tempList2);
+        if(tempList2.size() > 2) {
+            for (int i = 0; i < 10; i++) {
+                popularList.add(tempList2.get(i) + "");
+            }
+        }
+
+        /*
         final ArrayList<String> subjects = new ArrayList<>(), subjectsNew = new ArrayList<>(), ids = new ArrayList<>(), idsNew = new ArrayList<>(),
                 authors = new ArrayList<>(), authorsNew = new ArrayList<>(), likes = new ArrayList<>(), likesOld = new ArrayList<>(), dates = new ArrayList<>(), datesNew = new ArrayList<>();
         final ArrayList<Integer> indexs = new ArrayList<>();
-        /*
+
         for (int i = 0; i < parser.subjects.size(); i++) {
             ids.add(parser.ids.get(i));
             subjects.add(parser.subjects.get(i));
@@ -74,7 +98,7 @@ public class PopularItems_Fragment extends Fragment {
         for (int i = 0; i < parser.subjects.size(); i++) {
             ids.add(parser.ids.get(i));
         }
-        */
+
 
         //Parser parser = new Parser();
         //Toast.makeText(getContext(), parser.subjects.size() + "", Toast.LENGTH_LONG).show();
@@ -84,10 +108,11 @@ public class PopularItems_Fragment extends Fragment {
         ids.add("3");
         ids.add("4");
 
+
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, ids);
+        */
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, popularList);
         listView_popular.setAdapter(arrayAdapter);
-
-
         return view;
     }
 }
