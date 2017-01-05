ab9ed5ba54797ab86f4111ef2d6f0f691541ec9d
PiiiTeeeR <piter969@op.pl>
2017-01-05 21:28:27 +0100

    Splitowanie coordinates
    ss

--- app/src/main/java/com/civicproject/civicproject/LocationActivity.java
+++ app/src/main/java/com/civicproject/civicproject/LocationActivity.java
@@ -2,6 +2,9 @@ package com.civicproject.civicproject;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -25,25 +28,38 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class LocationActivity extends AppCompatActivity  {
 
 
-    String locations;
+    Double distance;
     Parser parser = new Parser();
     Button buttonAddProject;
-
+    Double locationX = Double.NaN, locationY = Double.NaN;
+    String splited[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scrolling_addproject);
 
         final ArrayList<String> locations = new ArrayList<>();
+        final ArrayList<String> X = new ArrayList<>();
+        final ArrayList<String> Y = new ArrayList<>();
       //  locations = parser.locations;
 
         for (int i = 0; i < parser.locations.size(); i++) {
+            splited = parser.locations.get(i).split("\\s");
             locations.add(parser.locations.get(i));
+            X.add(splited[0]);
+            Y.add(splited[1]);
+            double lat1 = 55, lon1 = 19.529735, lat2 = 51.730011, lon2 = 19.532406;
+            distance = haversine(lat1, lon1, lat2, lon2);
+            distance *= 1000;
         }
+
+
 
 
         buttonAddProject = (Button) findViewById(R.id.buttonAddProject);
@@ -56,6 +72,41 @@ public class LocationActivity extends AppCompatActivity  {
         });
 
     }
+    public static double  haversine(double lat1, double lng1, double lat2, double lng2) {
+        int r = 6371; // promień ziemi w kilometrach
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lng2 - lng1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                        * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = r * c;
+        return d;
+    }
+
+    private String getAddress(double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder
+                    .getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                android.location.Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i < returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress
+                            .append(returnedAddress.getAddressLine(i)).append(
+                            "\n");
+                }
+                strAdd = strReturnedAddress.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return strAdd;
+    }
+
 
 }
 
